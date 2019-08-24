54f950b8a5eff646c7f631423b245f227c3c3d28
rogerdavila <roger.davila@impetu.mx>
2019-08-23 19:03:38 -0500

    Add Eureka Clients - Configure 'items' microservice for work with EurekaClient - Configure 'productos' microservice for work with EurekaClient - Delete explicit configurations and imports for Ribbon of 'items' microservice due to Eureka already includes it

--- springboot-servicio-items/src/main/java/com/formacionbdi/springboot/app/items/SpringbootServicioItemsApplication.java
+++ springboot-servicio-items/src/main/java/com/formacionbdi/springboot/app/items/SpringbootServicioItemsApplication.java
@@ -2,10 +2,11 @@ package com.formacionbdi.springboot.app.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "servicio-productos")
+@EnableEurekaClient
+//@RibbonClient(name = "servicio-productos")
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemsApplication {
