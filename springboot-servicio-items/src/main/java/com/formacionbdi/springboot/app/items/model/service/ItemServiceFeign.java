27e6b74970bca52d01033cb0638d6892fc1f73ec
rogerdavila <roger.davila@impetu.mx>
2019-09-29 15:12:24 -0500

    Remove unnecessary imports and comments

--- springboot-servicio-items/src/main/java/com/formacionbdi/springboot/app/items/model/service/ItemServiceFeign.java
+++ springboot-servicio-items/src/main/java/com/formacionbdi/springboot/app/items/model/service/ItemServiceFeign.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import com.formacionbdi.springboot.app.items.clientes.ProductoClienteRest;
--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controller/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controller/ProductoController.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,13 +22,9 @@ public class ProductoController {
 	@Value("${eureka.instance.instance-id}")
 	private String port;
 	
-	@Autowired
-	private Environment env;
-
 	@GetMapping("/listar")
 	public ResponseEntity<List<Producto>> listar() {
 		return ResponseEntity.ok(productoService.findAll().stream().map(producto -> {
-			// producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 			producto.setPort(port);
 			return producto;
 		}).collect(Collectors.toList()));
@@ -38,7 +33,6 @@ public class ProductoController {
 	@GetMapping("/ver/{id}")
 	public ResponseEntity<Producto> detalle (@PathVariable Long id) throws Exception{
 		Producto producto = productoService.findById(id);
-		// producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 		producto.setPort(port);
 		
 		return ResponseEntity.ok(producto);
